09a6c5d951ae24771c4856d1ee5159599a076c83
Ossi Miilukangas <ossi.miilukangas@hotmail.com>
2020-11-28 00:10:32 +0200

    Started faceit stats feature, doesn't print anything yet

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const axios = require('axios').default;
 const token = require("./token.js");
 
-client.on('ready', () => {
+const prefix = "!";
+
+const faceitToken = "4839db51-1fbd-49c4-8a61-328cc2964122";
+const faceitUrl = "https://open.faceit.com/data/v4";
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
  
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on("message", msg => {
+  if(msg.author.bot) return;
+  if(!msg.content.startsWith(prefix)) return;
+
+  if(msg.content.startsWith(prefix + "faceit")) {
+    let playerId;
+    let stats = {
+      level: 0,
+      elo: 0,
+      kd: 0,
+      wr: 0,
+      matches: 0,
+      wins: 0,
+      hsPct: 0,
+      wStreak: 0
+    }
+
+    axios({
+      method: "GET",
+      url: faceitUrl + "/players",
+      headers: { Authorization: "Bearer " + faceitToken },
+      params: { nickname: msg.content.slice(7) }
+    }).then(res => {
+      playerId = res.data.player_id;
+      stats.level = res.data.games.csgo.skill_level;
+      stats.elo = res.data.games.csgo.faceit_elo;
+      axios({
+        method: "GET",
+        url: faceitUrl + "/players/" + playerId + "/stats/csgo",
+        headers: { Authorization: "Bearer " + faceitToken }
+      }).then(res => {
+        stats.kd = res.data.lifetime["Average K/D Ratio"];
+        stats.wr = res.data.lifetime["Win Rate %"];
+        stats.matches = res.data.lifetime.Matches;
+        stats.wins = res.data.lifetime.Wins;
+        stats.hsPct = res.data.lifetime["Average Headshots %"];
+        stats.wStreak = res.data.lifetime["Longest Win Streak"];
+
+        console.log(stats);
+      }).catch(error => {
+        console.log(error);
+      })
+
+    }).catch(error => {
+      console.log(error);
+    })
   }
 });
 
