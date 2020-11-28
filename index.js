879ae89d8acd24818daceceb58c800dc55e69e21
Ossi Miilukangas <ossi.miilukangas@hotmail.com>
2020-11-28 16:11:29 +0200

    Added command info response

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
 const axios = require('axios').default;
-const token = require("./token.js");
+const tokens = require("./tokens.js");
 
 const prefix = "!";
 
-const faceitToken = "4839db51-1fbd-49c4-8a61-328cc2964122";
+const faceitToken = tokens.faceitToken;
 const faceitUrl = "https://open.faceit.com/data/v4";
+const faceitColor = 0xff5500;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,50 +17,71 @@ client.on("message", msg => {
   if(msg.author.bot) return;
   if(!msg.content.startsWith(prefix)) return;
 
+  // faceit features
   if(msg.content.startsWith(prefix + "faceit")) {
-    let playerId;
-    let stats = {
-      level: 0,
-      elo: 0,
-      kd: 0,
-      wr: 0,
-      matches: 0,
-      wins: 0,
-      hsPct: 0,
-      wStreak: 0
-    }
+    let command = msg.content.split(" ");
 
-    axios({
-      method: "GET",
-      url: faceitUrl + "/players",
-      headers: { Authorization: "Bearer " + faceitToken },
-      params: { nickname: msg.content.slice(7) }
-    }).then(res => {
-      playerId = res.data.player_id;
-      stats.level = res.data.games.csgo.skill_level;
-      stats.elo = res.data.games.csgo.faceit_elo;
+    if(command[1] !== undefined) {
+      let player = {
+        id: 0,
+        level: 0,
+        elo: 0,
+        kd: 0,
+        wr: 0,
+        matches: 0,
+        wins: 0,
+        hsPct: 0,
+        wStreak: 0
+      }
+
       axios({
         method: "GET",
-        url: faceitUrl + "/players/" + playerId + "/stats/csgo",
-        headers: { Authorization: "Bearer " + faceitToken }
-      }).then(res => {
-        stats.kd = res.data.lifetime["Average K/D Ratio"];
-        stats.wr = res.data.lifetime["Win Rate %"];
-        stats.matches = res.data.lifetime.Matches;
-        stats.wins = res.data.lifetime.Wins;
-        stats.hsPct = res.data.lifetime["Average Headshots %"];
-        stats.wStreak = res.data.lifetime["Longest Win Streak"];
-
-        console.log(stats);
-      }).catch(error => {
-        console.log(error);
+        url: faceitUrl + "/players",
+        headers: { Authorization: "Bearer " + faceitToken },
+        params: { nickname: msg.content.slice(7) }
       })
+      .then(res => {
+        player.id = res.data.player_id;
+        player.level = res.data.games.csgo.skill_level;
+        player.elo = res.data.games.csgo.faceit_elo;
 
-    }).catch(error => {
-      console.log(error);
-    })
+        axios({
+          method: "GET",
+          url: faceitUrl + "/players/" + player.id + "/stats/csgo",
+          headers: { Authorization: "Bearer " + faceitToken }
+        })
+        .then(res => {
+          player.kd = res.data.lifetime["Average K/D Ratio"];
+          player.wr = res.data.lifetime["Win Rate %"];
+          player.matches = res.data.lifetime.Matches;
+          player.wins = res.data.lifetime.Wins;
+          player.hsPct = res.data.lifetime["Average Headshots %"];
+          player.wStreak = res.data.lifetime["Longest Win Streak"];
+
+          console.log(player);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+
+      })
+      .catch(error => {
+        console.log(error);
+        if(error.response.status === 404) {
+          msg.channel.send("**Faceit user not found.**");
+        }
+      })
+    }
+    // Command info
+    else {
+      const embed = new MessageEmbed()
+        .setTitle("Faceit features")
+        .setColor(faceitColor)
+        .setDescription('- **!faceit {username}**: Get stats\n- **!faceit -lm {username}**: Get scoreboard of your last match');
+      msg.channel.send(embed);
+    }
   }
 });
 
-client.login(token);
+client.login(tokens.discordToken);
 
